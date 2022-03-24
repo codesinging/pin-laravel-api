4321744f6f70799fad8af9a549f7f6b8985b5cc8
codesinging <codesinging@gmail.com>
2022-03-24 13:46:39 +0800

    新增后台管理独立路由文件：routes/admin.php

---
+++ routes/admin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+use App\Http\Controllers\Admin;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware(['auth:sanctum'])
+    ->group(function (){
+
+
+    });
