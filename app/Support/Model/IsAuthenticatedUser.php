92ae52a597d178de6b235241ee96929256851146
codesinging <codesinging@gmail.com>
2022-03-24 13:32:12 +0800

    增加模型方法：isAuthenticatedUser，判断一个用户是否当前认证的用户

---
+++ app/Support/Model/IsAuthenticatedUser.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Author: codesinging <codesinging@gmail.com>
+ * Github: https://github.com/codesinging
+ */
+
+namespace App\Support\Model;
+
+use Illuminate\Support\Facades\Auth;
+
+trait IsAuthenticatedUser
+{
+    /**
+     * 是否当前认证的管理员
+     *
+     * @return bool
+     */
+    public function isAuthenticatedUser(): bool
+    {
+        return Auth::id() === $this->attributes['id'];
+    }
+
+}
--- tests/Feature/Support/Model/AuthModelTest.php
+++ tests/Feature/Support/Model/AuthModelTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Support\Model;
 
 use App\Support\Model\AuthModel;
 use App\Support\Model\HelperTraits;
+use App\Support\Model\IsAuthenticatedUser;
 use Illuminate\Foundation\Auth\User;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -22,5 +23,7 @@ class AuthModelTest extends TestCase
         self::assertArrayHasKey(Notifiable::class, class_uses(AuthModel::class));
 
         self::assertArrayHasKey(HelperTraits::class, class_uses(AuthModel::class));
+
+        self::assertArrayHasKey(IsAuthenticatedUser::class, class_uses(AuthModel::class));
     }
 }
