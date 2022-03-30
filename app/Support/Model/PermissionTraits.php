ac951904576da495bf9b8ae9548996e5d3f3e255
codesinging <codesinging@gmail.com>
2022-03-30 13:04:49 +0800

    删除用户模型方法：getPermittedModels

--- app/Support/Model/PermissionTraits.php
+++ app/Support/Model/PermissionTraits.php
@@ -6,7 +6,6 @@
 
 namespace App\Support\Model;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Collection;
 
 trait PermissionTraits
@@ -21,22 +20,5 @@ trait PermissionTraits
     public function getPermissionsFrom(string $class): Collection
     {
         return $this->getAllPermissions()->filter(fn($permission) => str_starts_with($permission['name'], $class));
-    }
-
-    /**
-     * 获取拥有权限的指定的模型集合
-     *
-     * @param string $class
-     *
-     * @return Collection
-     */
-    public function getPermittedModels(string $class): Collection
-    {
-        $permissions = $this->getPermissionsFrom($class);
-
-        /** @var Model|AuthPermissionContract $model */
-        $model = new $class();
-
-        return $model->newQuery()->whereIn('permission_id', $permissions->pluck('id'))->get();
     }
 }
