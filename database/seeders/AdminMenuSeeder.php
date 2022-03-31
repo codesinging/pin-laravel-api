cf78039d704a0d29d8146a836229b5f7883d8c88
codesinging <codesinging@gmail.com>
2022-03-31 17:45:15 +0800

    修改默认菜单数据填充

--- database/seeders/AdminMenuSeeder.php
+++ database/seeders/AdminMenuSeeder.php
@@ -10,12 +10,10 @@ class AdminMenuSeeder extends Seeder
     private array $menus = [
         ['name' => '首页', 'path' => 'home', 'icon' => 'bi-house', 'default' => true, 'sort' => 1],
         ['name' => '系统管理', 'path' => 'system', 'icon' => 'bi-gear', 'opened' => true, 'sort' => 2, 'children' => [
+            ['name' => '菜单管理', 'path' => 'menus', 'icon' => 'bi-list', 'sort' => 1],
             ['name' => '管理员管理', 'path' => 'admins', 'icon' => 'bi-person', 'sort' => 2],
-            ['name' => '菜单管理', 'path' => 'menus', 'icon' => 'bi-list', 'sort' =>1],
-            ['name' => '权限管理', 'path' => 'authorizations', 'icon' => 'bi-locker', 'sort' => 3, 'children' => [
-                ['name' => '角色管理', 'path' => 'roles'],
-                ['name' => '权限管理', 'path' => 'auths'],
-            ]],
+            ['name' => '角色管理', 'path' => 'roles', 'icon' => 'bi-people', 'sort' => 3],
+            ['name' => '权限管理', 'path' => 'auths', 'icon' => 'bi-shield', 'sort' => 4],
         ]],
     ];
 
