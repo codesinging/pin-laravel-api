3d5cba1df8e80cb32daf9cc551a0049f21ee5d55
codesinging <codesinging@gmail.com>
2022-04-05 23:49:51 +0800

    更新菜单填充数据

--- database/seeders/AdminMenuSeeder.php
+++ database/seeders/AdminMenuSeeder.php
@@ -10,11 +10,11 @@ class AdminMenuSeeder extends Seeder
     private array $menus = [
         ['name' => '首页', 'path' => 'home', 'icon' => 'bi-house', 'default' => true, 'sort' => 9],
         ['name' => '系统管理', 'path' => 'system', 'icon' => 'bi-gear', 'opened' => true, 'sort' => 3, 'children' => [
-            ['name' => '菜单管理', 'path' => 'menus', 'icon' => 'bi-list', 'sort' => 1],
-            ['name' => '管理员管理', 'path' => 'admins', 'icon' => 'bi-person', 'sort' => 2],
-            ['name' => '角色管理', 'path' => 'roles', 'icon' => 'bi-people', 'sort' => 3],
-            ['name' => '页面管理', 'path' => 'pages', 'icon' => 'bi-file-earmark-text', 'sort' => 4],
-            ['name' => '路由管理', 'path' => 'routes', 'icon' => 'bi-shield-check', 'sort' => 4],
+            ['name' => '角色管理', 'path' => 'roles', 'icon' => 'bi-people', 'sort' => 99],
+            ['name' => '管理员管理', 'path' => 'admins', 'icon' => 'bi-person', 'sort' => 98],
+            ['name' => '菜单管理', 'path' => 'menus', 'icon' => 'bi-list', 'sort' => 97],
+            ['name' => '页面管理', 'path' => 'pages', 'icon' => 'bi-file-earmark-text', 'sort' => 96],
+            ['name' => '路由管理', 'path' => 'routes', 'icon' => 'bi-shield-check', 'sort' => 95],
         ]],
     ];
 
