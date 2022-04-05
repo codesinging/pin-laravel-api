36ef98ca5752dff4a1c87444a969c6ecf87b1f01
codesinging <codesinging@gmail.com>
2022-04-05 23:40:17 +0800

    更新菜单填充数据

--- database/seeders/AdminMenuSeeder.php
+++ database/seeders/AdminMenuSeeder.php
@@ -8,8 +8,8 @@ use Illuminate\Database\Seeder;
 class AdminMenuSeeder extends Seeder
 {
     private array $menus = [
-        ['name' => '首页', 'path' => 'home', 'icon' => 'bi-house', 'default' => true, 'sort' => 1],
-        ['name' => '系统管理', 'path' => 'system', 'icon' => 'bi-gear', 'opened' => true, 'sort' => 2, 'children' => [
+        ['name' => '首页', 'path' => 'home', 'icon' => 'bi-house', 'default' => true, 'sort' => 9],
+        ['name' => '系统管理', 'path' => 'system', 'icon' => 'bi-gear', 'opened' => true, 'sort' => 3, 'children' => [
             ['name' => '菜单管理', 'path' => 'menus', 'icon' => 'bi-list', 'sort' => 1],
             ['name' => '管理员管理', 'path' => 'admins', 'icon' => 'bi-person', 'sort' => 2],
             ['name' => '角色管理', 'path' => 'roles', 'icon' => 'bi-people', 'sort' => 3],
