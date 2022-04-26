d8425bbb161b3079ad900fb527fd388c35438fa1
codesinging <codesinging@gmail.com>
2022-04-26 23:41:40 +0800

    修改默认种子

--- database/seeders/AdminPageSeeder.php
+++ database/seeders/AdminPageSeeder.php
@@ -9,10 +9,12 @@ use Illuminate\Database\Seeder;
 class AdminPageSeeder extends Seeder
 {
     protected array $pages = [
-        ['name' => '仪表盘', 'path' => 'dashboard'],
-        ['name' => '后台菜单管理', 'path' => 'admin_menus'],
-        ['name' => '后台页面管理', 'path' => 'admin_pages'],
-        ['name' => '后台管理员管理', 'path' => 'admins'],
+        ['name' => '首页', 'path' => 'home'],
+        ['name' => '菜单管理', 'path' => 'menus'],
+        ['name' => '页面管理', 'path' => 'pages'],
+        ['name' => '角色管理', 'path' => 'roles'],
+        ['name' => '路由管理', 'path' => 'routes'],
+        ['name' => '管理员管理', 'path' => 'admins'],
     ];
 
     /**
@@ -20,7 +22,7 @@ class AdminPageSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(): void
     {
         foreach ($this->pages as $page) {
             AdminPage::new()->create($page);
