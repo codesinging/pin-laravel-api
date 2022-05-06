00eef38fc145152113d6adf51919529c84e538a5
codesinging <codesinging@gmail.com>
2022-05-06 12:07:23 +0800

    更新模型的静态方法

--- app/Support/Model/StaticMethods.php
+++ app/Support/Model/StaticMethods.php
@@ -6,22 +6,43 @@
 
 namespace App\Support\Model;
 
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
 trait StaticMethods
 {
-    public static function creates(array $attributes = []): Model|BaseModel
+    /**
+     * 创建模型
+     * @param array $attributes
+     *
+     * @return Model|BaseModel|$this
+     */
+    public static function creates(array $attributes = []): Model|BaseModel|static
     {
         return (new static())->create($attributes);
     }
 
-    public static function one(array|string $columns = ['*']): Model|BaseModel|null
+    /**
+     * 查找一个模型
+     * @param array|string $columns
+     *
+     * @return Model|$this|null
+     */
+    public static function one(array|string $columns = ['*']): Model|null|static
     {
         return (new static())->first($columns);
     }
 
-    public static function finds($id, $columns = ['*'])
+    /**
+     * 查找指定的模型
+     * @param $id
+     * @param string[] $columns
+     *
+     * @return Model|Collection|$this|null
+     */
+    public static function finds($id, array $columns = ['*']): Model|Collection|null|static
     {
         return (new static())->find($id, $columns);
     }
+
 }
